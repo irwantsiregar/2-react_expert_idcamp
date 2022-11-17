95a735e302a35c802edcddd1ab4b9e0a71ea709a
Sigadong <irwanto.regar30@gmail.com>
2022-11-18 00:41:23 +0700

    practice: combine reducers

--- src/index.js
+++ src/index.js
@@ -4,6 +4,23 @@
 
 import { createStore } from './redux';
 
+function todosReducer(todos = [], action = {}) {
+  if (action.type === 'ADD_TODO') {
+    return [...todos, action.payload];
+  }
+
+  if (action.type === 'DELETE_TODO') {
+    return todos.filter((todo) => todo.id !== action.payload.id);
+  }
+
+  if (action.type === 'TOGGLE_TODO') {
+    // eslint-disable-next-line max-len
+    return todos.map((todo) => (todo.id === action.payload.id ? { ...todo, complete: !todo.complete } : todo));
+  }
+
+  return todos;
+}
+
 function addTodoActionCreator({ id, text }) {
   return {
     type: 'ADD_TODO',
@@ -33,29 +50,51 @@ function toggleTodoActionCreator(id) {
   };
 }
 
-function todosReducer(todos = [], action = {}) {
-  if (action.type === 'ADD_TODO') {
-    return [...todos, action.payload];
-  }
-
-  if (action.type === 'DELETE_TODO') {
-    return todos.filter((todo) => todo.id !== action.payload.id);
-  }
-
-  if (action.type === 'TOGGLE_TODO') {
-    // eslint-disable-next-line max-len
-    return todos.map((todo) => (todo.id === action.payload.id ? { ...todo, complete: !todo.complete } : todo));
-  }
-
-  return todos;
+function addGoalActionCreator({ id, text }) {
+  return {
+    type: 'ADD_GOAL',
+    payload: {
+      id,
+      text,
+    },
+  };
 }
 
-const store = createStore(todosReducer);
+function deleteGoalActionCreator(id) {
+  return {
+    type: 'DELETE_GOAL',
+    payload: {
+      id,
+    },
+  };
+}
+
+function goalsReducer(goals = [], action = {}) {
+  if (action.type === 'ADD_GOAL') {
+    return [...goals, action.payload];
+  }
+
+  if (action.type === 'DELETE_GOAL') {
+    return goals.filter((goal) => goal.id !== action.payload.id);
+  }
+
+  return goals;
+}
+
+function rootReducer(state = {}, action = {}) {
+  return {
+    todos: todosReducer(state.todos, action),
+    goals: goalsReducer(state.goals, action),
+  };
+}
+
+const store = createStore(rootReducer);
 
 store.subscribe(() => {
   console.log('state changed!', store.getState());
 });
 
+// Trigger event To-do
 store.dispatch(
   addTodoActionCreator({
     id: 1,
@@ -82,3 +121,20 @@ store.dispatch(deleteTodoActionCreator(3));
 
 // mengubah Learn React menjadi complete
 store.dispatch(toggleTodoActionCreator(1));
+
+// Trigger event Goals
+store.dispatch(
+  addGoalActionCreator({
+    id: 1,
+    text: 'Get a Doctorate',
+  }),
+);
+
+store.dispatch(
+  addGoalActionCreator({
+    id: 2,
+    text: 'Be an Entrepreneur',
+  }),
+);
+
+store.dispatch(deleteGoalActionCreator(1));
