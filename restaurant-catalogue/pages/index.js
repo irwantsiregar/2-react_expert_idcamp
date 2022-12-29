e700961924062c52e6a6d72e3be5e225a2c25453
Sigadong <irwanto.regar30@gmail.com>
2022-12-30 00:31:59 +0700

    practice: building a static react site with next.js

---
+++ restaurant-catalogue/pages/index.js
@@ -0,0 +1,20 @@
+import RestaurantList from "../components/RestaurantsList";
+
+export default function Home({ restaurants }) {
+  return (
+    <div className='container'>
+      <RestaurantList restaurants={restaurants} />
+    </div>
+  )
+};
+
+export async function getStaticProps() {
+  const response = await fetch('https://restaurant-api.dicoding.dev/list');
+  const { restaurants } = await response.json();
+
+  return {
+    props: {
+      restaurants,
+    },
+  }
+}
